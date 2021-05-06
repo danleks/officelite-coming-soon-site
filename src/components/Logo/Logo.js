16324c8f505807308a0032664ddb96f7990b4036
danleks <danilevich.lks@gmail.com>
2021-05-06 22:30:15 +0200

    Create Logo.js

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LogoIcon from '../../assets/shared/logo.inline.svg';
+
+const Logo = () => {
+    return (
+        <div>
+            <LogoIcon />
+        </div>
+    )
+}
+
+export default Logo;
