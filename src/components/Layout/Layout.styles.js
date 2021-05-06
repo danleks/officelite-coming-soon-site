f2ff8c7eebfaf059924e777102158319815a3da9
danleks <danilevich.lks@gmail.com>
2021-05-06 22:30:03 +0200

    Update Layout styles

---
+++ src/components/Layout/Layout.styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import bgPatternFooter from '../../assets/home/bg-pattern-footer.svg';
+
+export const MainWrapperStyles = styled.div`
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+`;
+
+export const ContentWrapperStyles = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 32.7rem;
+    padding-top: 4.6rem;
+    border: 1px solid red;
+`;
