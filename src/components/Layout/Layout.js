00b536da42f5ede7de058766747992186dbd00a3
danleks <danilevich.lks@gmail.com>
2021-05-06 22:29:50 +0200

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,14 +2,19 @@ import React from 'react';
 import 'normalize.css';
 import GlobalStyles from '../../styles/GlobalStyles';
 import Typography from '../../styles/Typography';
-import Navigation from '../Navigation/Navigation';
+import { MainWrapperStyles, ContentWrapperStyles} from './Layout.styles';
+import Header from '../Header/Header';
 
 const Layout = ({ children }) => (
     <>
         <GlobalStyles />
         <Typography />
-        <Navigation />
-        {children}
+        <MainWrapperStyles>
+            <Header />
+            <ContentWrapperStyles>
+                {children}
+            </ContentWrapperStyles>
+        </MainWrapperStyles>
     </>
 );
 
