e6d2a8bbc434da1556dbe77e7850da4cb5a45cd7
danleks <danilevich.lks@gmail.com>
2021-05-08 20:31:48 +0200

    Create Hero.js

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { HeroSectionStyles, ContentWrapper, ChartIllustrationStyles, MainTitleStyles, SecondaryTitleStyles, HeroButtonStyles } from './Hero.styles';
+
+const Hero = () => {
+    return (
+        <HeroSectionStyles>
+            <ContentWrapper>
+                <ChartIllustrationStyles />
+            </ContentWrapper>
+            <ContentWrapper>
+                <MainTitleStyles className="mainTitle">A simple solution to complex tasks is coming soon</MainTitleStyles>
+                <SecondaryTitleStyles className="secondaryTitle">Say goodbye to inefficient juggling of multiple apps, teams, and projects. Officelite is the new collaboration platform built with an intuitive interface to improve productivity.</SecondaryTitleStyles>
+                <HeroButtonStyles>Get Started</HeroButtonStyles>
+            </ContentWrapper>
+        </HeroSectionStyles>
+    )
+}
+
+export default Hero;
