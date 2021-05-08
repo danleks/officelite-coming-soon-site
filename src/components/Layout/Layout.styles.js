76b472b620cbd88751f7494fd1fb5ba7459ac942
danleks <danilevich.lks@gmail.com>
2021-05-08 20:32:14 +0200

    Update Layout styles

--- src/components/Layout/Layout.styles.js
+++ src/components/Layout/Layout.styles.js
@@ -1,17 +1,27 @@
 import styled from 'styled-components';
-import bgPatternFooter from '../../assets/home/bg-pattern-footer.svg';
 
 export const MainWrapperStyles = styled.div`
+    position: relative;
     display: flex;
     justify-content: center;
-    height: 100vh;
+    max-width: 144rem;
+    margin: auto;
+    /* overflow: hidden; */
+    /* flex-direction: column; */
 `;
 
 export const ContentWrapperStyles = styled.div`
+    --pt: 4.6rem;
+    --width: 32.7rem;
     display: flex;
     flex-direction: column;
     align-items: center;
-    width: 32.7rem;
-    padding-top: 4.6rem;
-    border: 1px solid red;
+    width: var(--width);
+    padding-top: var(--pt);
+    @media(min-width: 768px) {
+        --width: 68.9rem;
+    }
+    @media(min-width: 1440px) {
+        --width: 110rem;
+    }
 `;
