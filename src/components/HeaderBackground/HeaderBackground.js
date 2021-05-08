7e1d383366c830d9641362a827c032688b26c352
danleks <danilevich.lks@gmail.com>
2021-05-08 20:31:30 +0200

    Create HeaderBackground.js

---
+++ src/components/HeaderBackground/HeaderBackground.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HeaderBackgroundStyles } from './HeaderBackground.styles.js';
+
+const Header = () => {
+    return (
+        <HeaderBackgroundStyles />
+    )
+}
+
+export default Header;
