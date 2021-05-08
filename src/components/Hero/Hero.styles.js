9ff482dc37423a6fff6a794b3ea1e916ae83eddc
danleks <danilevich.lks@gmail.com>
2021-05-08 20:31:57 +0200

    Create Hero styles

---
+++ src/components/Hero/Hero.styles.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import Button from '../Button/Button';
+import Svg from '../../assets/home/charts.inline.svg';
+
+export const HeroSectionStyles = styled.section`
+    --mgt: 8rem;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 6.4rem;
+    width: 100%;
+    margin-top: var(--mgt);
+    @media(min-width: 768px) {
+        flex-direction: row-reverse;
+        gap: 6.3rem;
+    }
+    @media(min-width: 1440px) {
+        gap: 9.5rem;
+    }
+`;
+
+export const ContentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ChartIllustrationStyles = styled(Svg)`
+    width: 17.1rem;
+    @media(min-width: 768px) {
+        width: 28.1rem;
+    }
+    @media(min-width: 1440px) {
+        width: 47.5rem;
+    }
+`;
+
+export const MainTitleStyles = styled.h1`
+    text-align: center;
+    @media(min-width: 768px) {
+        text-align: left;
+    }
+`;
+
+export const SecondaryTitleStyles = styled.h2`
+    --mgt: 2.4rem;
+    text-align: center;
+    margin-top: var(--mgt);
+    @media(min-width: 768px) {
+        text-align: left;
+    }
+`;
+
+export const HeroButtonStyles = styled(Button)`
+    margin-top: 2.4rem;
+    @media(min-width: 768px) {
+        align-self: flex-start;
+    }
+`;
