1f565d198c45eb7f273db7b3a6a5dc2f57270f11
danleks <danilevich.lks@gmail.com>
2021-05-11 22:59:14 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
+import Subscription from '../components/Subscription/Subscription';
 
 const HomePage = () => (
   <>
     <Hero />
+    <Subscription />
   </>
 );
 
