3d0b387c643c6535e362ee7a90459abbefc5524e
danleks <danilevich.lks@gmail.com>
2021-05-11 22:58:55 +0200

    Create Subscription.js

---
+++ src/components/Subscription/Subscription.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from '../Card/Card';
+import { SubscriptionStyles } from './Subscription.styles';
+
+const Subscription = () => {
+    return (
+        <SubscriptionStyles>
+            <Card />
+        </SubscriptionStyles>
+    )
+}
+
+export default Subscription;
