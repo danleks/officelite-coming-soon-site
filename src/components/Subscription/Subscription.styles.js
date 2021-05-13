3d42179a9d6b5378223056b11f7a8726aee139e4
danleks <danilevich.lks@gmail.com>
2021-05-13 22:38:26 +0200

    Update Subscription styles

--- src/components/Subscription/Subscription.styles.js
+++ src/components/Subscription/Subscription.styles.js
@@ -3,4 +3,7 @@ import styled from 'styled-components';
 export const SubscriptionStyles = styled.div`
     --mgt: 10rem;
     margin-top: var(--mgt);
+    display: flex;
+    flex-direction: column;
+    gap: 3.2rem;
 `;
