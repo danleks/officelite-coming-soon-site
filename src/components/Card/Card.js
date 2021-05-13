c90547e1b8a4ffd59b21996ce1d8573d6d4189dc
danleks <danilevich.lks@gmail.com>
2021-05-13 22:37:54 +0200

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,49 +1,27 @@
 import React from 'react';
-import { useStaticQuery, graphql } from "gatsby";
 import Button from '../Button/Button';
 import { CardStyles, CardInnerWrapperStyles, CardTitleStyles, CardPlanTypeStyles, CardPlanUserNumber, CardPlanDesc } from './Card.styles';
 
 
-const Card = () => {
-    const data = useStaticQuery(graphql`
-        query CardQuery {
-            allDatoCmsCard {
-                nodes {
-                    id
-                    price
-                    title
-                    cardColor
-                    usersAllowed
-                    desc {
-                        value
-                    }
-                }
-            }
-        }
-    `)
-    const cards = data.allDatoCmsCard.nodes;
+const Card = ({ id, price, title, usersAllowed, planColor, description1, description2, description3 }) => {
     return (
-       <>
-            {cards.map(({ id, price, title, usersAllowed, cardColor, desc }) => (
-                <CardStyles key={id} color={cardColor}>
-                    <CardInnerWrapperStyles mgt='40'>
-                        <CardTitleStyles>{title}</CardTitleStyles>
-                    </CardInnerWrapperStyles>
-                    <CardInnerWrapperStyles mgt='40'>
-                        <CardPlanTypeStyles>{price}</CardPlanTypeStyles>
-                        <CardPlanUserNumber>{usersAllowed}</CardPlanUserNumber>
-                    </CardInnerWrapperStyles>
-                    <CardInnerWrapperStyles mgt='56'>
-                        <CardPlanDesc>Basic document collaboration</CardPlanDesc>
-                        <CardPlanDesc>2 GB storage</CardPlanDesc>
-                        <CardPlanDesc>Great security and support</CardPlanDesc>
-                    </CardInnerWrapperStyles>  
-                    <CardInnerWrapperStyles mgt='32'>
-                        <Button light>Try for free</Button>
-                    </CardInnerWrapperStyles>     
-                </CardStyles>
-            ))}
-       </>
+        <CardStyles key={id} color={planColor}>
+            <CardInnerWrapperStyles mgt='40'>
+                <CardTitleStyles>{title}</CardTitleStyles>
+            </CardInnerWrapperStyles>
+            <CardInnerWrapperStyles mgt='40'>
+                <CardPlanTypeStyles>{price}</CardPlanTypeStyles>
+                <CardPlanUserNumber>{usersAllowed}</CardPlanUserNumber>
+            </CardInnerWrapperStyles>
+            <CardInnerWrapperStyles mgt='56'>
+                <CardPlanDesc color={planColor}>{description1}</CardPlanDesc>
+                <CardPlanDesc color={planColor}>{description2}</CardPlanDesc>
+                <CardPlanDesc color={planColor}>{description3}</CardPlanDesc>
+            </CardInnerWrapperStyles>  
+            <CardInnerWrapperStyles mgt='32'>
+                <Button light>Try for free</Button>
+            </CardInnerWrapperStyles>     
+        </CardStyles>
     )    
 }
 
