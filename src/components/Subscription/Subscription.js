2e0aebc6b70a9533580da8c5e8f9c17fb696745e
danleks <danilevich.lks@gmail.com>
2021-05-13 22:38:15 +0200

    Update Subscription.js

--- src/components/Subscription/Subscription.js
+++ src/components/Subscription/Subscription.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useStaticQuery, graphql } from "gatsby";
 import Card from '../Card/Card';
 import { SubscriptionStyles } from './Subscription.styles';
 
 const Subscription = () => {
+    const data = useStaticQuery(graphql`
+        query CardQuery {
+            allDatoCmsPlan {
+                nodes {
+                    id
+                    price
+                    title
+                    planColor
+                    order
+                    usersAllowed
+                    description1
+                    description2
+                    description3
+                }
+            }
+        }
+    `);
+    const plans = data.allDatoCmsPlan.nodes;
     return (
         <SubscriptionStyles>
-            <Card />
+            {plans
+                .sort((a,b) => a.order - b.order)
+                .map(plan => {
+                return <Card {...plan} />
+            })}
         </SubscriptionStyles>
     )
 }
