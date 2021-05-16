542e17bb18d330a1e8e12be31a32914a242afe35
danleks <danilevich.lks@gmail.com>
2021-05-16 13:36:44 +0200

    Update Card styles

--- src/components/Card/Card.styles.js
+++ src/components/Card/Card.styles.js
@@ -17,6 +17,16 @@ export const CardStyles = styled.div`
     @media(min-width: 768px) {
         width: 68.9rem;
         height: 31.6rem;
+        flex-direction: row;
+        flex-wrap: wrap;
+        padding: 4.8rem;
+        padding-right: 6.8rem;
+    }
+    @media(min-width: 1200px) {
+        width: 35rem;
+        height: 50.8rem;
+        flex-direction: column;
+        padding: 0;
     }
 `;
 
@@ -25,18 +35,59 @@ export const CardInnerWrapperStyles = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    margin-top: ${({ mgt }) => `${mgt}px`};
     &:nth-child(1) {
         gap: 0;
+        margin-top: 4rem;
+        @media(min-width: 768px) {
+            flex: 1 100%;
+            align-items: flex-start;
+            margin-top: 0;
+        }
+        @media(min-width: 1200px) {
+            flex: auto;
+            align-items: inherit;
+            margin-top: 4rem;
+        }
     }
     &:nth-child(2) {
         gap: 8px;
+        margin-top: 4rem;
+        @media(min-width: 768px) {
+            flex: 1;
+            align-items: flex-start;
+            margin-top: 0;
+        }
+        @media(min-width: 1200px) {
+            flex: auto;
+            align-items: inherit;
+            margin-top: 4rem;
+        }
     }
     &:nth-child(3) {
         gap: 1.6rem;
+        margin-top: 5.6rem;
+        @media(min-width: 768px) {
+            flex: 1;
+            margin-top: 0;
+        }
+        @media(min-width: 1200px) {
+            flex: auto;
+            margin-top: 5.6rem;
+        }
     }
     &:nth-child(4) {
         gap: 0;
+        margin-top: 3.2rem;
+        @media(min-width: 768px) {
+            flex: 1 100%;
+            align-items: flex-start;
+            margin-top: 0;
+        }
+        @media(min-width: 1200px) {
+            flex: auto;
+            align-items: inherit;
+            margin-top: 3.2rem;
+        }
     }
 `;
 
