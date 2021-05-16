9775d65fb3deca2fb02132bc48b482ddcbb62704
danleks <danilevich.lks@gmail.com>
2021-05-16 13:37:08 +0200

    Update Subscription styles

--- src/components/Subscription/Subscription.styles.js
+++ src/components/Subscription/Subscription.styles.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
-export const SubscriptionStyles = styled.div`
+export const SubscriptionStyles = styled.section`
     --mgt: 10rem;
     margin-top: var(--mgt);
     display: flex;
     flex-direction: column;
     gap: 3.2rem;
+    @media(min-width: 1200px) {
+        --mgt: 18.4rem;
+        flex-direction: row;
+        gap: 3rem;
+    }
 `;
