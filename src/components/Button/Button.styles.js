6dce52c0048fa94f34ecdb0ef55a6879983e0996
danleks <danilevich.lks@gmail.com>
2021-05-16 13:36:23 +0200

    Update Button styles

--- src/components/Button/Button.styles.js
+++ src/components/Button/Button.styles.js
@@ -9,10 +9,10 @@ export const ButtonStyles = styled.button`
     font-weight: 700;
     line-height: 2.8rem;
     box-shadow: 0px 25px 25px -10px rgba(63, 91, 194, 0.247159);
-    transition: background-color .15s ease-in, opacity .15s ease-in;
+    transition: background-color .15s ease-in, opacity .15s ease-in, color .15s ease-in;
     cursor: pointer;
 
-    ${props => props.blue && css`
+    ${props => props.buttonColor === 'blue' && css`
         background-color: var(--blue);
         color: var(--white);
         &:hover {
@@ -20,11 +20,19 @@ export const ButtonStyles = styled.button`
         }   
     `}
     
-    ${props => props.light && css`
+    ${props => props.buttonColor === 'light' && css`
         color: var(--blue);
         background-color: rgba(81, 117, 255, .15);
         &:hover {
             background-color: rgba(81, 117, 255, .25);
+        }   
+    `}
+
+    ${props => props.buttonColor === 'white' && css`
+        color: var(--blue);
+        background-color: var(--white);
+        &:hover {
+           color: var(--lightBlue);
         }   
     `}
 `;
