93ced291a49d7bd2358f1f897f9b1cd741757035
danleks <danilevich.lks@gmail.com>
2021-05-17 22:33:25 +0200

    Update HeaderBackground styles

--- src/components/HeaderBackground/HeaderBackground.styles.js
+++ src/components/HeaderBackground/HeaderBackground.styles.js
@@ -3,12 +3,12 @@ import bgPatternHeader from '../../assets/home/bg-pattern-header.svg';
 
 export const HeaderBackgroundStyles = styled.div`
     position: absolute;
-    top: -17rem;
+    top: 0;
     display: flex;
     justify-content: center;
     width: 100%;
     height: 45.8rem;
-    padding-top: 4.6rem;
+    /* padding-top: 4.6rem; */
     background-image: url(${bgPatternHeader});
     background-size: 45.8rem;
     background-repeat: no-repeat;
@@ -17,14 +17,16 @@ export const HeaderBackgroundStyles = styled.div`
         height: 66rem;
         background-size: 66rem;
         background-position: 0;
-        left: calc(51.4%);
-        top: -164px; 
+        /* left: calc(51.4%);
+        top: -164px;  */
     }
-    @media(min-width: 1440px) {
+    @media(min-width: 1200px) {
         height: 113.4rem;
         background-size: 113.4rem;
-        background-position: 0;
-        left: 656px;
-        top: -419px;
+        background-position: top -41.9rem right -35rem;
     }
+    /* @media(min-width: 1441px) {
+        background-size: 90rem;
+        background-position: top calc(90rem * -.37 + 15vw) right calc(90rem * -.31 + 15vw);
+    } */
 `;
