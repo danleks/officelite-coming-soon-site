b82abd24e45f841d6e362a96ac0a4805b29bc1d3
danleks <danilevich.lks@gmail.com>
2021-05-17 22:33:00 +0200

    Create FooterBackground.js

---
+++ src/components/FooterBackground/FooterBackground.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FooterBackgroundStyles } from './FooterBackground.styles';
+
+const FooterBackground = () => {
+    return (
+        <FooterBackgroundStyles />
+    )
+}
+
+export default FooterBackground;
