7476c9fa7bd7fd94c15595e89a91d5dcb0949bcd
danleks <danilevich.lks@gmail.com>
2021-05-18 22:31:52 +0200

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,23 +3,17 @@ import 'normalize.css';
 import GlobalStyles from '../../styles/GlobalStyles';
 import Typography from '../../styles/Typography';
 import { MainWrapperStyles, ContentWrapperStyles} from './Layout.styles';
-import HeaderBackground from '../HeaderBackground/HeaderBackground';
-import Footer from '../Footer/Footer';
-import Logo from '../Logo/Logo';
-import FooterBackground from '../FooterBackground/FooterBackground';
+import Logo from '../Logo/Logo'
 
 const Layout = ({ children }) => (
     <>
         <GlobalStyles />
         <Typography />
         <MainWrapperStyles>
-            <HeaderBackground />
             <ContentWrapperStyles>
             <Logo />
                 {children}
-            <Footer />
             </ContentWrapperStyles>
-            <FooterBackground />
         </MainWrapperStyles>
     </>
 );
