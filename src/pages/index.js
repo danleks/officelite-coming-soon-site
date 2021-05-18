15b6c19524d18e8241cede5a1f8d77fa77db781c
danleks <danilevich.lks@gmail.com>
2021-05-18 22:32:23 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
 import Subscription from '../components/Subscription/Subscription';
+import ComingUp from '../components/ComingUp/ComingUp';
+import HeaderBackground from '../components/HeaderBackground/HeaderBackground';
+import FooterBackground from '../components/FooterBackground/FooterBackground';
 
 const HomePage = () => (
   <>
+    <HeaderBackground />
     <Hero />
     <Subscription />
+    <ComingUp />
+    <FooterBackground />
   </>
 );
 
