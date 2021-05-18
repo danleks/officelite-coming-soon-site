2cb7a6f4cffdd6dac938a6c452e78d5fcf15cb3c
danleks <danilevich.lks@gmail.com>
2021-05-18 22:32:06 +0200

    Create SignupHero.js

---
+++ src/components/SignupHero/SignupHero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SignupHeroStyles, SignupHeroTitle, SignupHeroSubTitle } from './SignupHero.styles';
+
+const SignupHero = () => {
+    return (
+        <SignupHeroStyles>
+            <SignupHeroTitle>Work smarter. Save time.</SignupHeroTitle>
+            <SignupHeroSubTitle>Easily manage your projects. Get on the list and receive in-app perks available only to early subscribers. We are moving into final development and getting ready for official launch soon.</SignupHeroSubTitle>
+        </SignupHeroStyles>
+    )
+}
+
+export default SignupHero;
